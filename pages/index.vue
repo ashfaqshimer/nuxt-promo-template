2309071078c0d51998c8855f343e63db27300095
Shimer <ashfaq.shimer@itelasoft.com>
2019-10-22 17:29:40 +0530

    Seperate index page into components

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,49 @@
 <template>
-  <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        nuxt-promo-template
-      </h1>
-      <h2 class="subtitle">
-        My super Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+        <hero-section></hero-section>
+
+        <section class="section">
+            <div class="container">
+                <h1 class="title">Featured Courses</h1>
+                <div class="columns">
+                    <div class="column is-one-quarter">
+                        <course-card />
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+                <h1 class="title">Featured Articles</h1>
+                <div class="columns">
+                    <div class="column is-one-quarter">
+                        <article-card />
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
-  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import HeroSection from "@/components/HeroSection";
+import CourseCard from "@/components/CourseCard";
+import ArticleCard from "@/components/ArticleCard";
 export default {
-  components: {
-    Logo
-  }
-}
+    components: { HeroSection, CourseCard, ArticleCard }
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style scoped lang="scss">
+// card item
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+// card item end
+// hero
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
+// hero
+// Home page
 .links {
-  padding-top: 15px;
+    padding-top: 15px;
 }
 </style>
